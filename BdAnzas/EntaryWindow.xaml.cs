0e3425b129eded9b5007f1c78bc1e09b6431162d
Александр Зайбель <alexzaibel98@mail.ru>
2023-04-14 16:23:24 +0700

    Настроил проет. Изменил окно на EntaryWindow. Добавил пакеты MaterialDesign. Сделал в окне выбор проекта.

--- BdAnzas/EntaryWindow.xaml.cs
+++ BdAnzas/EntaryWindow.xaml.cs
@@ -18,9 +18,9 @@ namespace BdAnzas
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class EntaryWindow : Window
     {
-        public MainWindow()
+        public EntaryWindow()
         {
             InitializeComponent();
         }
