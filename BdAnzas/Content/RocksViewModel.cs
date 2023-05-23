d7921f522597d2845c1272a120f57ccc45d37664
Александр Зайбель <alexzaibel98@mail.ru>
2023-05-23 16:08:02 +0700

    Добавил пока классы для RocksView и его вьюМодельку

---
+++ BdAnzas/Content/RocksViewModel.cs
@@ -0,0 +1,13 @@
+﻿using BdAnzas.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BdAnzas.Content
+{
+    internal class RocksViewModel:ViewModelBase
+    {
+    }
+}
