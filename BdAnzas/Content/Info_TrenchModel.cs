05e3491c77c6f5fc29a3b53b23130ffa5f7df581
Александр Зайбель <alexzaibel98@mail.ru>
2023-06-05 14:24:35 +0700

    Добавил удаление из базы в "информации по канавам"

--- BdAnzas/Content/Info_TrenchModel.cs
+++ BdAnzas/Content/Info_TrenchModel.cs
@@ -10,6 +10,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 
 namespace BdAnzas.Content
@@ -29,7 +30,7 @@ namespace BdAnzas.Content
         }
 
         private InfoTrench _selectedInfoTrench;
-        private InfoTrench SelectedInfoTrench
+        public InfoTrench SelectedInfoTrench
         {
             get => _selectedInfoTrench;
             set => Set(ref _selectedInfoTrench, value);
@@ -46,8 +47,66 @@ namespace BdAnzas.Content
                    .Include(item => item.GeologNavigation)
                    .AsNoTracking().ToObservableCollection();
             }
-
+            DeleteCommand = new LamdaCommand(OnDeleteCommandExcuted, DeleteCommandExecute);
         }
 
+        /// <summary>
+        /// Команда удаления 
+        /// </summary>
+        public ICommand DeleteCommand { get; }
+        private bool DeleteCommandExecute(object p) => true;
+        private void OnDeleteCommandExcuted(object p)
+        {
+
+            try
+            {
+                if (SelectedInfoTrench != null)
+                {
+                    using (AnzasContext dbcontext = new AnzasContext())
+                    {
+                        InfoTrench row = dbcontext.InfoTrenches.FirstOrDefault(i => i.HoleId == SelectedInfoTrench.HoleId);
+                        if (row is not null)
+                        {
+                            var strmessege = @$"Вы действительно хотите удалить: {SelectedInfoTrench.HoleId}";
+                            var result = MessageBox.Show(strmessege, "Удаление", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                            if (result == MessageBoxResult.Yes)
+                            {
+
+                                MessageBox.Show("Удаление прошло успешно");
+                                dbcontext.InfoTrenches.Remove(row);
+                                dbcontext.SaveChanges();
+                                //InfoDrills.Remove(infodrills);
+                                Refrash();
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Не выбран объект для удаления", "Удаление", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                throw;
+            }
+
+
+
+        }
+        private void Refrash()
+        {
+            using (AnzasContext dbcontext = new AnzasContext())
+            {
+                InfoTrench.Clear();
+                InfoTrench = dbcontext.InfoTrenches
+                   .Include(p => p.PlaceSiteNavigation)
+                   .Include(item => item.TypeLcodeNavigation)
+                   .Include(item => item.GeologNavigation)
+                   .AsNoTracking().ToObservableCollection();
+            }
+        }
     }
 }
