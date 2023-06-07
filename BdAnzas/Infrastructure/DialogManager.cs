6b624f61196041f03d79fcbe7e8a7817d85e5593
Александр Зайбель <alexzaibel98@mail.ru>
2023-06-07 12:02:12 +0700

    Создал интерфейсы и класс для окна сообщений

---
+++ BdAnzas/Infrastructure/DialogManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace BdAnzas.Infrastructure
+{
+    public class DialogManager : IDialogManager
+    {
+      
+        public void ShowMessage(string message, string titlemes, MessageBoxButton button, MessageBoxImage image)
+        {
+            MessageBox.Show(message, titlemes, button, image);
+        }
+
+        public MessageBoxResult ShowMessageResult(string message, string titlemes, MessageBoxButton button, MessageBoxImage image)
+        {
+            var result = MessageBox.Show(message, titlemes, button, image);
+            return result;
+        }
+    }
+}
---
+++ BdAnzas/Infrastructure/IDialogManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace BdAnzas.Infrastructure
+{
+    public interface IDialogManager
+    {
+
+        void ShowMessage(string message, string titlemes , MessageBoxButton button , MessageBoxImage image);
+    }
+}
