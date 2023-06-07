3c053bd7b90a45fe481a0e76cc83086ef06b543f
Александр Зайбель <alexzaibel98@mail.ru>
2023-06-07 12:01:39 +0700

    Создал константы для навигации, и кнопок с картинками окна сообщения

---
+++ BdAnzas/Constants/InfoMessege.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace BdAnzas.Constants
+{
+    public class InfoMessege
+    {
+        public const MessageBoxImage Error = MessageBoxImage.Error;
+        public const MessageBoxImage Question = MessageBoxImage.Question;
+        public const MessageBoxImage Warning = MessageBoxImage.Warning;
+        public const MessageBoxImage Information = MessageBoxImage.Information;
+
+        public const MessageBoxButton OK = MessageBoxButton.OK;
+        public const MessageBoxButton OKCancel = MessageBoxButton.OKCancel;
+        public const MessageBoxButton YesNo = MessageBoxButton.YesNo;
+        public const MessageBoxButton YesNoCancel = MessageBoxButton.YesNoCancel;
+
+    }
+}
---
+++ BdAnzas/Constants/NavigationKeys.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BdAnzas.Constants
+{
+    public static class NavigationKeys
+    {
+        public const string InfoDrillKey = @"Информация по скважинам";
+        public const string InfoTrenchKey = @"Информация по канавам";
+        public const string InfoRoutehKey = @"Информация по маршрутам";
+        public const string InfoSurveyhKey = @"Инклинометрия скважин";
+        public const string InfoSyrveyTrenchKey = @"Инклинометрия канав";
+        public const string RocksKey = @"Литология скважин";
+        public const string RocksRouteKey = @"Литология маршрута";
+        public const string SamplesKey = @"Samples";
+        public const string AssayKey = @"Assay";
+    }
+}
