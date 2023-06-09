cc22c3e3fe9fb9c088751444d81f50c50be2cfa2
Александр Зайбель <alexzaibel98@mail.ru>
2023-06-09 11:38:10 +0700

    Сделал вывод литологии скважин, открытие окна редактирование и добавления элемента

---
+++ Anzas.DAL/Services/RockRepository.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anzas.DAL.Services
+{
+    public class RockRepository : IRepository<Rock>
+    {
+        private readonly AnzasContext _dbContext;
+
+        public RockRepository(AnzasContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Rock Add(Rock item)
+        {
+            if (item is null) throw new ArgumentNullException(nameof(item));
+            _dbContext.Rocks.Add(item);
+            _dbContext.SaveChanges();
+            return item;
+        }
+
+        public async Task AddAsync(Rock item, CancellationToken Cancel = default)
+        {
+            if (item is null) throw new ArgumentNullException(nameof(item));
+            _dbContext.Rocks.Add(item);
+            await _dbContext.SaveChangesAsync(Cancel).ConfigureAwait(false);
+        }
+
+        public IEnumerable<Rock> GetAll()
+        {
+            return _dbContext.Rocks
+                    .Include(item => item.GeologNavigation)
+                    .Include(item => item.RockCodeNavigation).AsNoTracking().ToList();
+        }
+
+        public Rock GetById(int id)
+        {
+            return _dbContext.Rocks.SingleOrDefault(i => i.Uid == id);
+        }
+
+        public async Task<Rock> GetByIdAsync(int id)
+        {
+            return await _dbContext.Rocks.FirstOrDefaultAsync(i => i.Uid == id);
+        }
+
+        public int Max()
+        {
+            return _dbContext.Rocks.Max(i => i.Uid);
+        }
+
+        public void Remove(int id)
+        {
+            var item = _dbContext.Rocks.FirstOrDefault(i => i.Uid == id);
+
+            _dbContext.Rocks.Remove(item);
+            _dbContext.SaveChanges();
+        }
+
+        public async Task RemoveAsync(int id, CancellationToken Cancel = default)
+        {
+            var item = _dbContext.Rocks.FirstOrDefault(i => i.Uid == id);
+
+            _dbContext.Rocks.Remove(item);
+            await _dbContext.SaveChangesAsync(Cancel).ConfigureAwait(false);
+        }
+
+        public void Update(Rock item)
+        {
+            if (item is null) throw new ArgumentNullException(nameof(item));
+            _dbContext.Rocks.Update(item);
+            _dbContext.SaveChanges();
+        }
+
+        public async Task UpdateAsync(Rock item, CancellationToken Cancel = default)
+        {
+            if (item is null) throw new ArgumentNullException(nameof(item));
+            _dbContext.Rocks.Update(item);
+            await _dbContext.SaveChangesAsync(Cancel).ConfigureAwait(false);
+        }
+    }
+}
--- BdAnzas/Content/ViewModel/RocksViewModel.cs
+++ BdAnzas/Content/ViewModel/RocksViewModel.cs
@@ -1,13 +1,118 @@
-﻿using BdAnzas.Base;
+﻿using Anzas.DAL;
+using Anzas.DAL.Services;
+using BdAnzas.Base;
+using BdAnzas.Commands;
+using BdAnzas.Constants;
+using BdAnzas.Content.Windows;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace BdAnzas.Content.ViewModel
 {
     internal class RocksViewModel : ViewModelBase
     {
+        RockRepository _rockRepository; 
+        private ObservableCollection<Rock> _rocks;
+
+        public ObservableCollection<Rock> Rocks
+        {
+            get => _rocks;
+            set
+            {
+                _rocks = value;
+                OnPropertyChanged("Rocks");
+
+            }
+        }
+        private Rock _selectedRocks;
+    
+
+        public Rock SelectedRocks
+        {
+            get => _selectedRocks;
+            set => Set(ref _selectedRocks, value);
+        }
+
+
+
+
+
+        public RocksViewModel()
+        {
+
+            AnzasContext anzasContext = new AnzasContext();
+              _rockRepository= new RockRepository(anzasContext);
+            Rocks = new ObservableCollection<Rock>(_rockRepository.GetAll());
+
+        }
+
+
+        private ICommand _editItemCommand;
+        public ICommand EditItemCommand
+        {
+            get
+            {
+                if (_editItemCommand == null)
+                {
+                    _editItemCommand = new RelayCommand(param => OpenWindowEdit());
+                }
+                return _editItemCommand;
+            }
+        }
+
+
+        private ICommand _addItemCommand;
+        public ICommand AddItemCommand
+        {
+            get
+            {
+                if (_addItemCommand == null)
+                {
+
+                    _addItemCommand = new RelayCommand(param => OpenWindowAdd());
+                }
+                return _addItemCommand;
+            }
+        }
+
+        /// <summary>
+        /// Метод для редактирования
+        /// </summary>
+        private void OpenWindowEdit()
+        {
+            AddWindow window = new AddWindow();
+            if (SelectedRocks != null)
+            {
+                window.DataContext = new AddEditWindowViewModel(NavigationKeys.RocksKey, SelectedRocks.Uid);
+                if (window.ShowDialog() == false)
+                {
+                    Rocks.Clear();
+                    Rocks = new ObservableCollection<Rock>(_rockRepository.GetAll());
+                }
+            }
+
+        }
+        /// <summary>
+        /// Метод для добавления нового элемента
+        /// </summary>
+        private void OpenWindowAdd()
+        {
+            AddWindow window = new AddWindow();
+
+            window.DataContext = new AddEditWindowViewModel(NavigationKeys.RocksKey);
+            if (window.ShowDialog() == false)
+            {
+                Rocks.Clear();
+                Rocks = new ObservableCollection<Rock>(_rockRepository.GetAll());
+            }
+
+        }
+
+
     }
 }
