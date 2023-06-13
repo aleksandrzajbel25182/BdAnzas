4e559cd09a25ea909e9ab2973d507a4c2dfbecf0
Александр Зайбель <alexzaibel98@mail.ru>
2023-06-13 12:29:20 +0700

    Сделал вывод информации в таблицу по маршрутам

---
+++ Anzas.DAL/Services/InfoRouteRepository.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anzas.DAL.Services
+{
+    public class InfoRouteRepository : IRepository<InfoRoute>
+    {
+        private readonly AnzasContext _dbContext;
+
+        public InfoRouteRepository(AnzasContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public InfoRoute Add(InfoRoute item)
+        {
+            if (item is null) throw new ArgumentNullException(nameof(item));
+            _dbContext.InfoRoutes.Add(item);
+            _dbContext.SaveChanges();
+            return item;
+        }
+
+        public async Task AddAsync(InfoRoute item, CancellationToken Cancel = default)
+        {
+            if (item is null) throw new ArgumentNullException(nameof(item));
+            _dbContext.InfoRoutes.Add(item);
+            await _dbContext.SaveChangesAsync(Cancel).ConfigureAwait(false);
+        }
+
+        public IEnumerable<InfoRoute> GetAll()
+        {
+            return _dbContext.InfoRoutes
+                .Include(p => p.PlaceSiteNavigation)
+                .Include(item => item.TypeLcodeNavigation)
+                .Include(item => item.GeologNavigation).AsNoTracking().ToList();
+        }
+
+        public InfoRoute GetById(int id)
+        {
+            return _dbContext.InfoRoutes.SingleOrDefault(i => i.Uid == id);
+        }
+
+        public async Task<InfoRoute> GetByIdAsync(int id)
+        {
+            return await _dbContext.InfoRoutes.FirstOrDefaultAsync(i => i.Uid == id);
+        }
+
+        public int Max()
+        {
+            return _dbContext.InfoRoutes.Max(i => i.Uid);
+        }
+
+        public void Remove(int id)
+        {
+            var item = _dbContext.InfoRoutes.FirstOrDefault(i => i.Uid == id);
+
+            _dbContext.InfoRoutes.Remove(item);
+            _dbContext.SaveChanges();
+        }
+
+        public async Task RemoveAsync(int id, CancellationToken Cancel = default)
+        {
+            var item = _dbContext.InfoRoutes.FirstOrDefault(i => i.Uid == id);
+
+            _dbContext.InfoRoutes.Remove(item);
+            await _dbContext.SaveChangesAsync(Cancel).ConfigureAwait(false);
+        }
+
+        public void Update(InfoRoute item)
+        {
+            if (item is null) throw new ArgumentNullException(nameof(item));
+            _dbContext.InfoRoutes.Update(item);
+            _dbContext.SaveChanges();
+        }
+
+        public async Task UpdateAsync(InfoRoute item, CancellationToken Cancel = default)
+        {
+            if (item is null) throw new ArgumentNullException(nameof(item));
+            _dbContext.InfoRoutes.Update(item);
+            await _dbContext.SaveChangesAsync(Cancel).ConfigureAwait(false);
+        }
+    }
+}
+
--- BdAnzas/Content/ViewModel/Info_Route_ViewModel.cs
+++ BdAnzas/Content/ViewModel/Info_Route_ViewModel.cs
@@ -1,29 +1,126 @@
-﻿using BdAnzas.Base;
+﻿using Anzas.DAL;
+using Anzas.DAL.Services;
+using BdAnzas.Base;
+using BdAnzas.Commands;
+using BdAnzas.Constants;
+using BdAnzas.Content.Windows;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace BdAnzas.Content.ViewModel
 {
     internal  class Info_Route_ViewModel:ViewModelBase
     {
+        private InfoRouteRepository _infoRouteRepository;
+
 
         #region Свойства
+
+        /// <summary>
+        /// Информация по маршрутам
+        /// </summary>
+        private ObservableCollection<InfoRoute> _routes;
+        public ObservableCollection<InfoRoute> Routes
+        {
+            get => _routes;
+            set => Set(ref _routes, value);
+        }
+
+        /// <summary>
+        /// Выделенный элемент инф. по маршрутам
+        /// </summary>
+        private InfoRoute _selectedRoute;
+        public  InfoRoute SelectedRoute
+        {
+            get => _selectedRoute;
+            set => Set(ref _selectedRoute, value);
+        }
+        
+
 
         #endregion
 
 
         public Info_Route_ViewModel()
         {
+            AnzasContext anzasContext = new AnzasContext();
+            _infoRouteRepository = new InfoRouteRepository(anzasContext);
+            Routes = new ObservableCollection<InfoRoute>(_infoRouteRepository.GetAll());
+
 
         }
 
         #region Команды
 
+
+        private ICommand _editItemCommand;
+        public ICommand EditItemCommand
+        {
+            get
+            {
+                if (_editItemCommand == null)
+                {
+                    _editItemCommand = new RelayCommand(param => OpenWindowEdit());
+                }
+                return _editItemCommand;
+            }
+        }
+
+
+        private ICommand _addItemCommand;
+        public ICommand AddItemCommand
+        {
+            get
+            {
+                if (_addItemCommand == null)
+                {
+
+                    _addItemCommand = new RelayCommand(param => OpenWindowAdd());
+                }
+                return _addItemCommand;
+            }
+        }
+
+
         #endregion
 
+        /// <summary>
+        /// Метод для редактирования
+        /// </summary>
+        private void OpenWindowEdit()
+        {
+            AddWindow window = new AddWindow();
+            if (SelectedRoute != null)
+            {
+                window.DataContext = new AddEditWindowViewModel(NavigationKeys.InfoRoutehKey, SelectedRoute.Uid);
+                if (window.ShowDialog() == false)
+                {
+                    Routes.Clear();
+                    Routes = new ObservableCollection<InfoRoute>(_infoRouteRepository.GetAll());
+                }
+            }
+
+        }
+        /// <summary>
+        /// Метод для добавления нового элемента
+        /// </summary>
+        private void OpenWindowAdd()
+        {
+            AddWindow window = new AddWindow();
+
+            window.DataContext = new AddEditWindowViewModel(NavigationKeys.InfoRoutehKey);
+            if (window.ShowDialog() == false)
+            {
+                Routes.Clear();
+                Routes = new ObservableCollection<InfoRoute>(_infoRouteRepository.GetAll());
+            }
+
+        }
 
     }
 }
